b9b822526705a598d638e758723a78b71724ccec
Jakub Klímek <klimek@ksi.mff.cuni.cz>
2016-02-14 15:48:27 +0100

    Update based on experience with the ETL presentation

--- js/init.js
+++ js/init.js
@@ -2,8 +2,10 @@
   $(function(){
 
 	// Initialize collapse button
-	//$(".button-collapse").sideNav();
+	$(".button-collapse").sideNav();
 	// Initialize collapsible (uncomment the line below if you use the dropdown variation)
 	//$('.collapsible').collapsible();
+	$('.scrollspy').scrollSpy();
+	$('.toc-wrapper').pushpin({ top: $('.toc-wrapper').offset() ? $('.toc-wrapper').offset().top : 0 });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
